fefa2ba4601798015c60c1be09127315f6a7d64f
Srivatsa <srivatsa.workspace@gmail.com>
2024-09-02 21:06:11 +0530

    Added bash script to run inside EC2 - will clone and download requirements

---
+++ install_flask_app.sh
@@ -0,0 +1,7 @@
+sudo yum update -y
+sudo yum install git python3 -y
+sudo yum install python3-pip -y
+git clone https://github.com/SrivatsaRv/one2n-vatsa.git
+cd one2n-vatsa/
+pip3 install -r requirements.txt
+echo "You are now set to go"
