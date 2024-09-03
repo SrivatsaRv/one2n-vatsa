19a6b49c19ddd4a7f38a305b9b8238b9081fa740
Srivatsa <srivatsa.workspace@gmail.com>
2024-09-03 15:17:40 +0530

    modified install flask-app.sh file

--- install_flask_app.sh
+++ install_flask_app.sh
@@ -1,10 +1,3 @@
 sudo apt update
 sudo apt install python3 python3-pip -y
-pip3 install -r requirements.txt
-
-sudo cp flask-app.service /etc/systemd/system
-sudo systemctl daemon reload
-sudo systemctl start flask-app.service
-sudo systemctl enable flask-app.service
-sudo systemctl status flask-app.service
-echo "You are now set to go, Flask service is running systemd service as well!"
+pip3 install -r requirements.txt --break-system-packages
